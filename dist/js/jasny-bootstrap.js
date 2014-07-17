c3d6049da2bbe535a4843a0fd04e5cc066b15640
Benjamin Dawson <palcoy0@gmail.com>
2014-07-17 17:08:04 +1200

    auto hide on any click

--- dist/js/jasny-bootstrap.js
+++ dist/js/jasny-bootstrap.js
@@ -86,17 +86,17 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
     this.options  = $.extend({}, OffCanvas.DEFAULTS, options)
     this.state    = null
     this.placement = null
-    
+
     if (this.options.recalc) {
       this.calcClone()
       $(window).on('resize', $.proxy(this.recalc, this))
     }
-    
-    if (this.options.autohide)
+
+    if (this.options.autohide && !this.options.modal)
       $(document).on('click', $.proxy(this.autohide, this))
 
     if (this.options.toggle) this.toggle()
-    
+
     if (this.options.disablescrolling) {
         this.options.disableScrolling = this.options.disablescrolling
         delete this.options.disablescrolling
@@ -120,38 +120,38 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
       case 'bottom': return this.$element.outerHeight()
     }
   }
-  
+
   OffCanvas.prototype.calcPlacement = function () {
     if (this.options.placement !== 'auto') {
         this.placement = this.options.placement
         return
     }
-    
+
     if (!this.$element.hasClass('in')) {
       this.$element.css('visiblity', 'hidden !important').addClass('in')
-    } 
-    
+    }
+
     var horizontal = $(window).width() / this.$element.width()
     var vertical = $(window).height() / this.$element.height()
-        
+
     var element = this.$element
     function ab(a, b) {
       if (element.css(b) === 'auto') return a
       if (element.css(a) === 'auto') return b
-      
+
       var size_a = parseInt(element.css(a), 10)
       var size_b = parseInt(element.css(b), 10)
-  
+
       return size_a > size_b ? b : a
     }
-    
+
     this.placement = horizontal >= vertical ? ab('left', 'right') : ab('top', 'bottom')
-      
+
     if (this.$element.css('visibility') === 'hidden !important') {
       this.$element.removeClass('in').css('visiblity', '')
     }
   }
-  
+
   OffCanvas.prototype.opposite = function (placement) {
     switch (placement) {
       case 'top':    return 'bottom'
@@ -160,18 +160,18 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
       case 'right':  return 'left'
     }
   }
-  
+
   OffCanvas.prototype.getCanvasElements = function() {
     // Return a set containing the canvas plus all fixed elements
     var canvas = this.options.canvas ? $(this.options.canvas) : this.$element
-    
+
     var fixed_elements = canvas.find('*').filter(function() {
       return $(this).css('position') === 'fixed'
     }).not(this.options.exclude)
-    
+
     return canvas.add(fixed_elements)
   }
-  
+
   OffCanvas.prototype.slide = function (elements, offset, callback) {
     // Use jQuery animation if CSS transitions aren't supported
     if (!$.support.transition) {
@@ -182,15 +182,15 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
 
     var placement = this.placement
     var opposite = this.opposite(placement)
-    
+
     elements.each(function() {
       if ($(this).css(placement) !== 'auto')
         $(this).css(placement, (parseInt($(this).css(placement), 10) || 0) + offset)
-      
+
       if ($(this).css(opposite) !== 'auto')
         $(this).css(opposite, (parseInt($(this).css(opposite), 10) || 0) - offset)
     })
-    
+
     this.$element
       .one($.support.transition.end, callback)
       .emulateTransitionEnd(350)
@@ -203,12 +203,12 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
     if ($('body').data('offcanvas-style') === undefined) {
       $('body').data('offcanvas-style', $('body').attr('style') || '')
     }
-      
+
     $('body').css('overflow', 'hidden')
 
     if ($('body').width() > bodyWidth) {
       var padding = parseInt($('body').css(prop), 10) + $('body').width() - bodyWidth
-      
+
       setTimeout(function() {
         $('body').css(prop, padding)
       }, 1)
@@ -225,14 +225,14 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
 
   OffCanvas.prototype.show = function () {
     if (this.state) return
-    
+
     var startEvent = $.Event('show.bs.offcanvas')
     this.$element.trigger(startEvent)
     if (startEvent.isDefaultPrevented()) return
 
     this.state = 'slide-in'
     this.calcPlacement();
-    
+
     var elements = this.getCanvasElements()
     var placement = this.placement
     var opposite = this.opposite(placement)
@@ -252,13 +252,13 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
         $(this).css(placement, 0)
       }
     })
-    
+
     if (this.options.disableScrolling) this.disableScrolling()
     if (this.options.modal) this.toggleBackdrop()
-    
+
     var complete = function () {
       if (this.state != 'slide-in') return
-      
+
       this.state = 'slid'
 
       elements.removeClass('canvas-sliding').addClass('canvas-slid')
@@ -286,12 +286,12 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
 
     var complete = function () {
       if (this.state != 'slide-out') return
-      
+
       this.state = null
       this.placement = null
-      
+
       this.$element.removeClass('in')
-      
+
       elements.removeClass('canvas-sliding')
       elements.add(this.$element).add('body').each(function() {
         $(this).attr('style', $(this).data('offcanvas-style')).removeData('offcanvas-style')
@@ -304,7 +304,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
     if (this.options.modal) this.toggleBackdrop()
 
     elements.removeClass('canvas-slid').addClass('canvas-sliding')
-    
+
     setTimeout($.proxy(function() {
       this.slide(elements, offset, $.proxy(complete, this))
     }, this), 1)
@@ -326,6 +326,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
       if (doAnimate) this.$backdrop[0].offsetWidth // force reflow
 
       this.$backdrop.addClass('in')
+      this.$backdrop.on('click.bs', $.proxy(this.autohide, this))
 
       doAnimate ?
         this.$backdrop
@@ -363,21 +364,22 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
 
   OffCanvas.prototype.recalc = function () {
     if (this.$calcClone.css('display') === 'none' || (this.state !== 'slid' && this.state !== 'slide-in')) return
-    
+
     this.state = null
     this.placement = null
     var elements = this.getCanvasElements()
-    
+
     this.$element.removeClass('in')
-    
+
     elements.removeClass('canvas-slid')
     elements.add(this.$element).add('body').each(function() {
       $(this).attr('style', $(this).data('offcanvas-style')).removeData('offcanvas-style')
     })
   }
-  
+
   OffCanvas.prototype.autohide = function (e) {
-    if ($(e.target).closest(this.$element).length === 0) this.hide()
+    // if ($(e.target).closest(this.$element).length === 0) this.hide()
+    this.hide();
   }
 
   // OFFCANVAS PLUGIN DEFINITION
